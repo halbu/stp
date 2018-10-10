b69cd6eb42103d8908dbc4a4d72748d0e68eae3a
halbu <fstream.c@gmail.com>
2018-10-10 21:05:13 +0100

    Updates

---
+++ precache-manifest.f481606f290952b6632c14c2ed39dc0a.js
@@ -0,0 +1,566 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d74cc5e3f1ba26669a771ba6a91f90d",
+    "url": "/stp/static/media/ItemKitConveyorSorter.7d74cc5e.png"
+  },
+  {
+    "revision": "e3d0f5c64438c5a74b03",
+    "url": "/stp/static/css/main.509b9991.chunk.css"
+  },
+  {
+    "revision": "0649fa9f3646d8461037",
+    "url": "/stp/static/js/1.0649fa9f.chunk.js"
+  },
+  {
+    "revision": "66d339ab9cf5bc7f1b4e",
+    "url": "/stp/static/js/runtime~main.66d339ab.js"
+  },
+  {
+    "revision": "ee13f98d212b2eac85db3bb1a5b45f3e",
+    "url": "/stp/static/media/ApplianceChemistryStation.ee13f98d.png"
+  },
+  {
+    "revision": "5bad69cdf7450c0a818d73648b063d85",
+    "url": "/stp/static/media/Circuitboard.5bad69cd.png"
+  },
+  {
+    "revision": "c3a047004819d809d01e6494344aaab1",
+    "url": "/stp/static/media/DynamicAirConditioner.c3a04700.png"
+  },
+  {
+    "revision": "674b25934c747ffa48dc3f86fb1804d3",
+    "url": "/stp/static/media/DynamicGPR.674b2593.png"
+  },
+  {
+    "revision": "8215c9c50f2f672da71aa52d18e33683",
+    "url": "/stp/static/media/DynamicGasCanisterAir.8215c9c5.png"
+  },
+  {
+    "revision": "14447f59a3fdb10999379b000a397ac6",
+    "url": "/stp/static/media/DynamicGasCanisterCarbonDioxide.14447f59.png"
+  },
+  {
+    "revision": "b9bd669d5173213fba7bae6d94465477",
+    "url": "/stp/static/media/DynamicGasCanisterEmpty.b9bd669d.png"
+  },
+  {
+    "revision": "aade57e23b81156c03b497388b68d081",
+    "url": "/stp/static/media/DynamicGasCanisterFuel.aade57e2.png"
+  },
+  {
+    "revision": "4bfe5c1c15171a6172f2dbce417a4e57",
+    "url": "/stp/static/media/DynamicGasCanisterHydrogen.4bfe5c1c.png"
+  },
+  {
+    "revision": "fe7b868fc5482277aeb6e3f4e56c3aaa",
+    "url": "/stp/static/media/DynamicGasCanisterNitrogen.fe7b868f.png"
+  },
+  {
+    "revision": "8215c9c50f2f672da71aa52d18e33683",
+    "url": "/stp/static/media/DynamicGasCanisterOxygen.8215c9c5.png"
+  },
+  {
+    "revision": "b7ae18abcdac4e5d4741133af8729bd8",
+    "url": "/stp/static/media/DynamicGasCanisterPropane.b7ae18ab.png"
+  },
+  {
+    "revision": "622cb1637fc10d7f51c549c5d529b9e3",
+    "url": "/stp/static/media/DynamicGasCanisterVolatiles.622cb163.png"
+  },
+  {
+    "revision": "4e0426f6330aa6e47742bc949beb50e7",
+    "url": "/stp/static/media/DynamicGasCanisterWater.4e0426f6.png"
+  },
+  {
+    "revision": "69b4e2db4ea551d5ba8ba0f807c8e5f2",
+    "url": "/stp/static/media/DynamicGenerator.69b4e2db.png"
+  },
+  {
+    "revision": "1d5566254478a7c3db85f294c9d767d0",
+    "url": "/stp/static/media/DynamicHydroponics.1d556625.png"
+  },
+  {
+    "revision": "bccc48ac20ee262a969b78cf670c0630",
+    "url": "/stp/static/media/DynamicScrubber.bccc48ac.png"
+  },
+  {
+    "revision": "8852795903cc8e8cef4f8c3a874cc73a",
+    "url": "/stp/static/media/ItemCableCoil.88527959.png"
+  },
+  {
+    "revision": "1fd3d3a88e418f13894bc75dd01290ad",
+    "url": "/stp/static/media/ItemCableCoilHeavy.1fd3d3a8.png"
+  },
+  {
+    "revision": "200d7058d131d3864f578ebf1006b8df",
+    "url": "/stp/static/media/ItemChuteBin.200d7058.png"
+  },
+  {
+    "revision": "7da61a5c2a09766bbcf9734346562cb0",
+    "url": "/stp/static/media/ItemChuteCorner.7da61a5c.png"
+  },
+  {
+    "revision": "376ab00e4ad1fd1342ab165632fde920",
+    "url": "/stp/static/media/ItemChuteJunction.376ab00e.png"
+  },
+  {
+    "revision": "e7f99ce85d05017f7b2742a905a4b15d",
+    "url": "/stp/static/media/ItemChuteOutlet.e7f99ce8.png"
+  },
+  {
+    "revision": "432ecb3c2ef02ba8346c3800dab375ea",
+    "url": "/stp/static/media/ItemChuteStraight.432ecb3c.png"
+  },
+  {
+    "revision": "642d227082ef7d16001952756b51fa6f",
+    "url": "/stp/static/media/ItemCoalOre.642d2270.png"
+  },
+  {
+    "revision": "a312a92dd2f9f58438850944dc2c4600",
+    "url": "/stp/static/media/ItemCompositeFloorGrating.a312a92d.png"
+  },
+  {
+    "revision": "5680e6aa8e09051c18ac955ca3405648",
+    "url": "/stp/static/media/ItemConsole.5680e6aa.png"
+  },
+  {
+    "revision": "777e5a9bdd4eff8b145df6a8d7989d64",
+    "url": "/stp/static/media/ItemCopperIngot.777e5a9b.png"
+  },
+  {
+    "revision": "7da34f3f2c0a253558147019471b3700",
+    "url": "/stp/static/media/ItemCopperOre.7da34f3f.png"
+  },
+  {
+    "revision": "8822412b72ad6611ba57624ed6647ec7",
+    "url": "/stp/static/media/ItemCorn.8822412b.png"
+  },
+  {
+    "revision": "7a00a5bbfca96568e313a164029d26ee",
+    "url": "/stp/static/media/ItemDuctTape.7a00a5bb.png"
+  },
+  {
+    "revision": "d185712d04db922c4bd41b84bf5514b3",
+    "url": "/stp/static/media/ItemDynamicAirCon.d185712d.png"
+  },
+  {
+    "revision": "41a8a52ff04092b40cb90bf499f5b946",
+    "url": "/stp/static/media/ItemDynamicScrubber.41a8a52f.png"
+  },
+  {
+    "revision": "47fb84ef6ac34af6862e8dcda2f6d0e1",
+    "url": "/stp/static/media/ItemEggCarton.47fb84ef.png"
+  },
+  {
+    "revision": "72db6a0ef3c3c6da22fded338e680c06",
+    "url": "/stp/static/media/ItemElectrumIngot.72db6a0e.png"
+  },
+  {
+    "revision": "503652fb6f49ef1eab7f14677839f518",
+    "url": "/stp/static/media/ItemEvaSuit.503652fb.png"
+  },
+  {
+    "revision": "07c07a4db84cf46f05f092c9a30fb8db",
+    "url": "/stp/static/media/ItemFern.07c07a4d.png"
+  },
+  {
+    "revision": "4f371e39f83c963ca9d9628a3683cbf5",
+    "url": "/stp/static/media/ItemFireExtinguisher.4f371e39.png"
+  },
+  {
+    "revision": "a38284b753d3a220edb1f37856a9f658",
+    "url": "/stp/static/media/ItemFlashingLight.a38284b7.png"
+  },
+  {
+    "revision": "e29c96d16ae0a5a81361ccedadfabb30",
+    "url": "/stp/static/media/ItemFlour.e29c96d1.png"
+  },
+  {
+    "revision": "1d4889488487ad55d9435aaa57df4233",
+    "url": "/stp/static/media/ItemFlowerBlue.1d488948.png"
+  },
+  {
+    "revision": "3a9962aa02412e28c978bc70dd50a1ae",
+    "url": "/stp/static/media/ItemFlowerGreen.3a9962aa.png"
+  },
+  {
+    "revision": "dc4ae8ffc2ef4da8ebb9aa9cfacb2989",
+    "url": "/stp/static/media/ItemFlowerOrange.dc4ae8ff.png"
+  },
+  {
+    "revision": "ec9469f0555000ea22e6f815b0ffdf85",
+    "url": "/stp/static/media/ItemFlowerRed.ec9469f0.png"
+  },
+  {
+    "revision": "eea3b8916e6c8fbee0daa9e2abf3ac7a",
+    "url": "/stp/static/media/ItemFlowerYellow.eea3b891.png"
+  },
+  {
+    "revision": "387485b367d4e8c29fd8e468cfd0b338",
+    "url": "/stp/static/media/ItemFries.387485b3.png"
+  },
+  {
+    "revision": "32706b7709a30fa71a816f20034fc739",
+    "url": "/stp/static/media/ItemGasCanisterChlorine.32706b77.png"
+  },
+  {
+    "revision": "83031462c6d19edd7ad0c33e669b6fc9",
+    "url": "/stp/static/media/ItemGasCanisterHydrogen.83031462.png"
+  },
+  {
+    "revision": "9297c15f243b23d5b9110a5564acc7b1",
+    "url": "/stp/static/media/ItemGasFilterChlorine.9297c15f.png"
+  },
+  {
+    "revision": "275881220a46dee5c44416973c964366",
+    "url": "/stp/static/media/ItemGasFilterHydrogen.27588122.png"
+  },
+  {
+    "revision": "71df05e49de570ac7adb958312332dca",
+    "url": "/stp/static/media/ItemGasSensor.71df05e4.png"
+  },
+  {
+    "revision": "f2e65d40735c82b178f2aefd7762a849",
+    "url": "/stp/static/media/ItemGoldIngot.f2e65d40.png"
+  },
+  {
+    "revision": "4bba54b6dd0775121772ac7d628a7f52",
+    "url": "/stp/static/media/ItemGoldOre.4bba54b6.png"
+  },
+  {
+    "revision": "fb0a4c19078fc299f80d396002f5452c",
+    "url": "/stp/static/media/ItemHardJetpack.fb0a4c19.png"
+  },
+  {
+    "revision": "10277b652ad86597c5d88797dae03f1d",
+    "url": "/stp/static/media/ItemHardSuit.10277b65.png"
+  },
+  {
+    "revision": "9812cc05ed6fd073e0fee1504bb848a1",
+    "url": "/stp/static/media/ItemIgniter.9812cc05.png"
+  },
+  {
+    "revision": "20d37eb0e948a20ad8a09b330beb7fa3",
+    "url": "/stp/static/media/ItemInsulation.20d37eb0.png"
+  },
+  {
+    "revision": "cbe9aa709fc4ac15dde0f5a1bbdc87ac",
+    "url": "/stp/static/media/ItemIronIngot.cbe9aa70.png"
+  },
+  {
+    "revision": "8ba7baff9f2506515d50920311cbd791",
+    "url": "/stp/static/media/ItemIronOre.8ba7baff.png"
+  },
+  {
+    "revision": "136d86fafd571092ca0481ff9b405604",
+    "url": "/stp/static/media/ItemKitAirConditioner.136d86fa.png"
+  },
+  {
+    "revision": "b264f32f2c6e7bf118d3f2cdedf26b9d",
+    "url": "/stp/static/media/ItemKitArcFurnace.b264f32f.png"
+  },
+  {
+    "revision": "5aa6a3fffca4804e28415d3069380ba7",
+    "url": "/stp/static/media/ItemKitAtmospherics.5aa6a3ff.png"
+  },
+  {
+    "revision": "99dc117e153815bcd742bc0c6ccde447",
+    "url": "/stp/static/media/ItemKitAutolathe.99dc117e.png"
+  },
+  {
+    "revision": "a1684c82afa3d50716d54ac55f9e971a",
+    "url": "/stp/static/media/ItemKitBasket.a1684c82.png"
+  },
+  {
+    "revision": "f6b57b175cf974d3a08dd97d830b182d",
+    "url": "/stp/static/media/ItemKitCentrifuge.f6b57b17.png"
+  },
+  {
+    "revision": "3acd894e32520bcedd19e34c7bcc6716",
+    "url": "/stp/static/media/ItemKitConveyor.3acd894e.png"
+  },
+  {
+    "revision": "e3d0f5c64438c5a74b03",
+    "url": "/stp/static/js/main.e3d0f5c6.chunk.js"
+  },
+  {
+    "revision": "0fab5c423d46a368a371d4571d80a788",
+    "url": "/stp/static/media/ItemKitCrate.0fab5c42.png"
+  },
+  {
+    "revision": "2baf537bd0cb7ecd727652ad2bafee46",
+    "url": "/stp/static/media/ItemKitCrateMount.2baf537b.png"
+  },
+  {
+    "revision": "15fa33e50215d72e0cbb5d0907d0d27f",
+    "url": "/stp/static/media/ItemKitDynamicGenerator.15fa33e5.png"
+  },
+  {
+    "revision": "2a9eef486a0cfc304dce1c150485f2d6",
+    "url": "/stp/static/media/ItemKitDynamicHydroponics.2a9eef48.png"
+  },
+  {
+    "revision": "37378270227094381a0c77b9eb9cdf09",
+    "url": "/stp/static/media/ItemKitElectronicsPrinter.37378270.png"
+  },
+  {
+    "revision": "ac78d303d760972e123318be57ae9bb3",
+    "url": "/stp/static/media/ItemKitElevator.ac78d303.png"
+  },
+  {
+    "revision": "6dfdb2eecd81fe1a5771dc7257690050",
+    "url": "/stp/static/media/ItemKitFabricator.6dfdb2ee.png"
+  },
+  {
+    "revision": "23198adcb1cad9955b47717bc4c97cea",
+    "url": "/stp/static/media/ItemKitFiltrationMachine.23198adc.png"
+  },
+  {
+    "revision": "954745b43346899e0d5274ed0e872d39",
+    "url": "/stp/static/media/ItemKitFurnace.954745b4.png"
+  },
+  {
+    "revision": "290d58c4c78eb26e1679f96810b21b0b",
+    "url": "/stp/static/media/ItemKitGasGenerator.290d58c4.png"
+  },
+  {
+    "revision": "06c7d7f7733a8017b610ac056bca7494",
+    "url": "/stp/static/media/ItemKitHydraulicPipeBender.06c7d7f7.png"
+  },
+  {
+    "revision": "e780bbb567d2be5c93695196a5fe4f17",
+    "url": "/stp/static/media/ItemKitHydroponicStation.e780bbb5.png"
+  },
+  {
+    "revision": "23198adcb1cad9955b47717bc4c97cea",
+    "url": "/stp/static/media/ItemKitOreProcessor.23198adc.png"
+  },
+  {
+    "revision": "84dd7dfa17c28e3f6a6928ae4a9d3328",
+    "url": "/stp/static/media/ItemKitOrganicsPrinter.84dd7dfa.png"
+  },
+  {
+    "revision": "1128228ce84e94b4f32d1405134c9032",
+    "url": "/stp/static/media/ItemKitRegulator.1128228c.png"
+  },
+  {
+    "revision": "cd32ffe7adee2e14d1f6f2179c9cd570",
+    "url": "/stp/static/media/ItemKitSleeper.cd32ffe7.png"
+  },
+  {
+    "revision": "048c6102d448bc0c4fb0e2346be8d941",
+    "url": "/stp/static/media/ItemKitSolarPanel.048c6102.png"
+  },
+  {
+    "revision": "e77fbb0dac4e23ed0d2523f4545675ca",
+    "url": "/stp/static/media/ItemKitSpeaker.e77fbb0d.png"
+  },
+  {
+    "revision": "77fa6be62c164598a0a5ec531f29fe19",
+    "url": "/stp/static/media/ItemKitToolManufactory.77fa6be6.png"
+  },
+  {
+    "revision": "1031d22ee8ec73c9b052851b9550db72",
+    "url": "/stp/static/media/ItemKitTransformer.1031d22e.png"
+  },
+  {
+    "revision": "ac9bb21ea9fe8c5638ffe49d3eec8119",
+    "url": "/stp/static/media/ItemKitTransformerSmall.ac9bb21e.png"
+  },
+  {
+    "revision": "a1500ee9ec50bb0c98d19bbfd1dd4f91",
+    "url": "/stp/static/media/ItemKitTurret.a1500ee9.png"
+  },
+  {
+    "revision": "860ed02471a774b773ba363309a3c7bc",
+    "url": "/stp/static/media/ItemKitWallIron.860ed024.png"
+  },
+  {
+    "revision": "e9ab0e0ba3fc8df807adbafab464dd5b",
+    "url": "/stp/static/media/ItemMilk.e9ab0e0b.png"
+  },
+  {
+    "revision": "523996d331b10eac2ccabdffdcd74736",
+    "url": "/stp/static/media/ItemMiningDrill.523996d3.png"
+  },
+  {
+    "revision": "04669b7e80c088260c1dc0a88e0bcfbb",
+    "url": "/stp/static/media/ItemMotionSensor.04669b7e.png"
+  },
+  {
+    "revision": "9c51f578381cc163185f304f2010ea9f",
+    "url": "/stp/static/media/ItemMuffin.9c51f578.png"
+  },
+  {
+    "revision": "3a1f25ea7c1e76a9c19aa366edd20bc7",
+    "url": "/stp/static/media/ItemNickelIngot.3a1f25ea.png"
+  },
+  {
+    "revision": "fd7614ddd48eb7964fba6c9c618197e9",
+    "url": "/stp/static/media/ItemPassiveVent.fd7614dd.png"
+  },
+  {
+    "revision": "034e83175121653b78e2532b168e7935",
+    "url": "/stp/static/media/ItemPipeCrossJunction5.034e8317.png"
+  },
+  {
+    "revision": "c9cae4648e05c4897e078480d846e9ce",
+    "url": "/stp/static/media/ItemPipeCrossJunction6.c9cae464.png"
+  },
+  {
+    "revision": "6b1092e293cc47382e9be75c8790523d",
+    "url": "/stp/static/media/ItemPipeDigitalValve.6b1092e2.png"
+  },
+  {
+    "revision": "2c1c7feab030b0463c705a1c580d6874",
+    "url": "/stp/static/media/ItemPipeGasMixer.2c1c7fea.png"
+  },
+  {
+    "revision": "f3d96ac3ca80f53e77d2c03e4b23fe8d",
+    "url": "/stp/static/media/ItemPipeGasPump.f3d96ac3.png"
+  },
+  {
+    "revision": "98d04925ccbfa03c689ebde05acfcd4b",
+    "url": "/stp/static/media/ItemPipeMeter.98d04925.png"
+  },
+  {
+    "revision": "079baea5bd5820c797de556b3a360190",
+    "url": "/stp/static/media/ItemPipeVolumePump.079baea5.png"
+  },
+  {
+    "revision": "2165f75d87a58666284474645d3fdf88",
+    "url": "/stp/static/media/ItemPumpkin.2165f75d.png"
+  },
+  {
+    "revision": "7827ad52a191d041576e6a51b30cb4b4",
+    "url": "/stp/static/media/ItemPumpkinPie.7827ad52.png"
+  },
+  {
+    "revision": "e9cf4480511ccfea5d4bf9ceb5f03d53",
+    "url": "/stp/static/media/ItemReagentMix.e9cf4480.png"
+  },
+  {
+    "revision": "83e4e23edb2d0c38a2a4ef7935ac8a4d",
+    "url": "/stp/static/media/ItemRice.83e4e23e.png"
+  },
+  {
+    "revision": "50ab5be7e8ff88c3299c24c22e9eea35",
+    "url": "/stp/static/media/ItemSensorKit.50ab5be7.png"
+  },
+  {
+    "revision": "c5bfd257687f346ddee08fbcc3656bb7",
+    "url": "/stp/static/media/ItemSilverIngot.c5bfd257.png"
+  },
+  {
+    "revision": "4e427012e578821529cabb6488690975",
+    "url": "/stp/static/media/ItemSoybean.4e427012.png"
+  },
+  {
+    "revision": "d4fc2757828ed444033df03731b12b29",
+    "url": "/stp/static/media/ItemSpaceCleaner.d4fc2757.png"
+  },
+  {
+    "revision": "eaefffcb3259089f7d535a0d35f15826",
+    "url": "/stp/static/media/ItemSpacepack.eaefffcb.png"
+  },
+  {
+    "revision": "04fb814732323322f3f6327a23bf1bcc",
+    "url": "/stp/static/media/ItemSprayCanBlack.04fb8147.png"
+  },
+  {
+    "revision": "3949e01d89ae6178fdeb90dcc145fd82",
+    "url": "/stp/static/media/ItemSprayCanBlue.3949e01d.png"
+  },
+  {
+    "revision": "83d6b6f0e47d2b3c8613482e2295c7b0",
+    "url": "/stp/static/media/ItemSprayCanBrown.83d6b6f0.png"
+  },
+  {
+    "revision": "38be977de34f4603bc7a09cdb4214621",
+    "url": "/stp/static/media/ItemSprayCanGreen.38be977d.png"
+  },
+  {
+    "revision": "cb54647c307509f7217a5e8d5199c5e9",
+    "url": "/stp/static/media/ItemSprayCanGrey.cb54647c.png"
+  },
+  {
+    "revision": "e84a76d1faef75ad21c835d04f798035",
+    "url": "/stp/static/media/ItemSprayCanKhaki.e84a76d1.png"
+  },
+  {
+    "revision": "7507d88e2244d6c7ce4480a2c7d69e0d",
+    "url": "/stp/static/media/ItemSprayCanOrange.7507d88e.png"
+  },
+  {
+    "revision": "c368b599165d5b85b66e26258f6474f9",
+    "url": "/stp/static/media/ItemSprayCanPink.c368b599.png"
+  },
+  {
+    "revision": "61d360388d0c6eb2a707a0f768dcdeb6",
+    "url": "/stp/static/media/ItemSprayCanPurple.61d36038.png"
+  },
+  {
+    "revision": "a8982588b473cbea1f4a617fbd9bb2de",
+    "url": "/stp/static/media/ItemSprayCanRed.a8982588.png"
+  },
+  {
+    "revision": "f2ae1ef92cccd13084ffd4da4947935c",
+    "url": "/stp/static/media/ItemSprayCanWhite.f2ae1ef9.png"
+  },
+  {
+    "revision": "ef5b4a7177d4dd86936efe32cd4db633",
+    "url": "/stp/static/media/ItemSprayCanYellow.ef5b4a71.png"
+  },
+  {
+    "revision": "8e1ff222324b9c993e3b710f8198df84",
+    "url": "/stp/static/media/ItemStacker.8e1ff222.png"
+  },
+  {
+    "revision": "3fef09e50666f52594328373d135865e",
+    "url": "/stp/static/media/ItemSteelIngot.3fef09e5.png"
+  },
+  {
+    "revision": "df3177f87abcb5b217354d3b0065c4e7",
+    "url": "/stp/static/media/ItemSuitStorage.df3177f8.png"
+  },
+  {
+    "revision": "989bbf36f1d25c4e65f5e8c6420b7b51",
+    "url": "/stp/static/media/ItemTomatoSoup.989bbf36.png"
+  },
+  {
+    "revision": "4c4619af7f5f3b757fdf5d9099cd2fa7",
+    "url": "/stp/static/media/ItemUraniumOre.4c4619af.png"
+  },
+  {
+    "revision": "f1b119126c7b21469b36ae1bee32de05",
+    "url": "/stp/static/media/ItemWallLight.f1b11912.png"
+  },
+  {
+    "revision": "449dade5e5058157981e9dc8341eebec",
+    "url": "/stp/static/media/ItemWheat.449dade5.png"
+  },
+  {
+    "revision": "db218e488124965eb7041adeb0883bdb",
+    "url": "/stp/static/media/MotherboardLogic.db218e48.png"
+  },
+  {
+    "revision": "71766d1aef01e8aec75a562348e2e1ad",
+    "url": "/stp/static/media/OrganBrain.71766d1a.png"
+  },
+  {
+    "revision": "b252656a396dbaea571a61fa029c934a",
+    "url": "/stp/static/media/OrganLungs.b252656a.png"
+  },
+  {
+    "revision": "576c3a358fcf753338eab5afec567949",
+    "url": "/stp/static/media/UniformOrangeJumpSuit.576c3a35.png"
+  },
+  {
+    "revision": "6d0d973305e5e7f5a7e16435357c887f",
+    "url": "/stp/static/media/list-item-hover.6d0d9733.png"
+  },
+  {
+    "revision": "cb93cc000586076e7877537a53860386",
+    "url": "/stp/index.html"
+  }
+];
